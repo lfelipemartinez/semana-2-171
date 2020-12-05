dac8f1494df98fa4e8e346100c68045c327b9527
Luis <lfelipemartinez07@gmail.com>
2020-12-05 16:31:01 -0500

    agregando header link

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="app">
+    <page-header></page-header>
   </div>
 </template>
 
 <script>
-
+import PageHeader from './components/PageHeader.vue'
 export default {
   name: 'App',
+  components:{
+    PageHeader,
+  }
 }
 </script>
 
