67b272cc4618bf506ad20bee8f07eb326e290813
IngCristianMarin <cristianymarins@gmail.com>
2020-12-06 01:29:32 -0500

    agregando banner cristian

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <page-header></page-header>
+    <page-banner></page-banner>
     <section-api> </section-api>    
      <Footer></Footer>
   </div>
@@ -10,12 +11,14 @@
 import PageHeader from './components/PageHeader.vue'
 import SectionApi from './components/SectionApi';
 import Footer from './components/Footer.vue';
+import PageBanner from "./components/PageBanner.vue";
 export default {
   name: 'App',
   components:{
     PageHeader,
      SectionApi,
-     Footer
+     Footer,
+     PageBanner
   }
 }
 </script>
---
+++ src/components/PageBanner.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="demo " class="carousel slide" data-ride="carousel">
+
+  <!-- Indicators -->
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+
+  <!-- The slideshow -->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="img-fluid container-fluid p-0" src="../../public/images/1.jpg" alt="Los Angeles"> 
+    </div>
+    <div class="carousel-item">
+      <img class="img-fluid container-fluid p-0" src="../../public/images/2.jpg" alt="Chicago">
+    </div>
+    <div class="carousel-item">
+      <img class="img-fluid container-fluid p-0" src="../../public/images/3.jpg" alt="New York">
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+
+</div>
+</template>
+<script>
+export default {
+    name:"PageBanner" 
+}
+</script>
