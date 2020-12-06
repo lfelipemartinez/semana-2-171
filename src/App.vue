27805efd9f0a9eed3f9dc199408c0fbd25abeb89
Juan Camilo Bazurto <camilobzt@gmail.com>
2020-12-05 22:00:50 -0500

    commit2

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
+    <Footer></Footer>
   </div>
+
 </template>
 
 <script>
 
+import Footer from './components/Footer.vue'
+
 export default {
   name: 'App',
+  components:{
+    Footer
+  }
+
 }
 </script>
 
