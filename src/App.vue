880915320a0360d20106ab80f996e3e63d4fb1fa
yeseniasanchez <75147615+yeseniasanchez@users.noreply.github.com>
2020-12-06 04:43:09 -0300

    cambios sesion miembros

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,77 @@
 <template>
   <div id="app">
     <page-header></page-header>
-    <section-api> </section-api>    
+    <section-api> </section-api>
+    
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col mt-5" v-for="(item, index) of team " :key="index">
+        <team-card v-bind:member= "item"></team-card>
+      </div>
+    </div>
+    </div>
   </div>
 </template>
 
 <script>
-import PageHeader from './components/PageHeader.vue'
-import SectionApi from './components/SectionApi';
+import PageHeader from "./components/PageHeader.vue";
+import SectionApi from "./components/SectionApi";
+import TeamCard from './components/TeamCard.vue';
+const imgLink = require('../public/images/ndml.jpg');
+const imgLink1 = require('../public/images/cyms.jpg');
+const imgLink2 = require('../public/images/yss.jpg');
+const imgLink3 = require('../public/images/jcba.jpg');
+const imgLink4 = require('../public/images/lfm.jpg');
 export default {
-  name: 'App',
-  components:{
+  name: "App",
+  components: {
     PageHeader,
-     SectionApi,
+    SectionApi,
+    TeamCard
+  },
+  data (){
+    return{
+      team: [
+        {
+          codigo: 1,
+          nombre: "Neiry Dayan Martinez Lara",
+          descripcion: "Ingeniera Biomédica de la Universidad Manuela Beltran               Pasatiempos: Jugar fútbol y ver series.",
+          rol: "Desarrollador Frontend",
+          image: imgLink
+        },
+        {
+          codigo: 2,
+          nombre: "Cristian Yesid Marin ",
+          descripcion: " Ingeniero Electronico de la Universidad Central                         Pasatiempos: Practicar deportes como el futbol de salón y el baloncesto, ir al cine con su familia.",
+          rol: "Desarrollador backend",
+          image: imgLink1
+        },
+        {
+          codigo: 3,
+          nombre: "Yesenia Sánchez Sánchez",
+          descripcion: "Administradora de empresas de la Universidad del Valle            Pasatiempos: Viajar.",
+          rol: "Desarrollador backend",
+          image: imgLink2
+        },
+        {
+          codigo: 4,
+          nombre: "Juan Camilo Bazurto Arias",
+          descripcion: "Estudiante de Ingenieria en Sistemas de la Escuela Colombiana de Ingenieria Julio Garavito, Pasatiempos: Leer, hacer ejercicio y practicar futbol.",
+          rol: "Desarrollador Frontend",
+          image: imgLink3
+        },
+        {
+          codigo: 5,
+          nombre: "Luis Felipe Martínez",
+          descripcion: "Ingeniero en Sistemas de la Universidad Autónoma del Caribe, Pasatiempos: jugar voleyball, trotar, salir a comer, leer manga,ver anime y visitar a mi novia",
+          rol: "Scrum Master",
+          image: imgLink4
+        }
+      ]
+    }
   }
-}
+};
+
 </script>
+
 
--- src/components/TeamCard.vue
+++ src/components/TeamCard.vue
@@ -1,13 +1,23 @@
 <template>
+    <div class="card-text-white bg-dark">
+        <div class="d-flex justify-content-center p-2">
+            <img v-bind:src= "member.image" alt="fotografia del miembro" width="200px" height="200px">
+        </div>
     <div>
-
+        <h2 class="miembro__nombre">{{ member.nombre }}</h2>
+        <p class="card-text text-center text-white">{{ member.descripcion }}</p>
+        <p class="card-text text-center text-white"><span> Rol: </span> {{ member.rol}}<p>
+        <p class="card-text text-center text-white"><span> Codigo: </span> {{ member.codigo}}<p>
+        </p>
+                
+    </div>
     </div>
 </template>
 
 <script>
     export default {
         name: "TeamCard",
-        props: ['member']
+        props: ["member"]
     }
 </script>
 
